99d3c2983ac178838279af31abd8c96fdde3033e
mcoletto <mcoletto@udesa.edu.ar>
2022-04-07 19:16:14 -0300

    fix db prod

--- Prog2-G3/db/index.js
+++ Prog2-G3/db/index.js
@@ -2,76 +2,75 @@ let data = {
     usuario: 3,
     productos: [
         {
-            modelo: iPhone 13 Pro
-            marca: Apple
-            anio: 2021
-            color: azul sierra
-            memoria: 64 GB
-            tamanio: 6.1”
+            modelo: 'iPhone 13 Pro',
+            marca: 'Apple',
+            anio: 2021,
+            color: 'azul sierra',
+            memoria: 64,
+            tamanio: 6.1,
         }, {
-            modelo: iPhone 13 Pro Max
-            marca: Apple
-            anio: 2021
-            color: grafito
-            memoria: 512 GB
-            tamanio: 6.7”
+            modelo: 'iPhone 13 Pro Max',
+            marca: Apple,
+            anio: 2021,
+            color: grafito,
+            memoria: 512,
+            tamanio: 6.7,
         }, {
-            modelo: Galaxy S22 Ultra
-            marca: Samsung
-            anio: 2022
-            color: burgundy
-            memoria: 256 GB
-            tamanio: 6.8”
+            modelo: 'Galaxy S22 Ultra',
+            marca: 'Samsung',
+            anio: 2022,
+            color: 'burgundy',
+            memoria: 256,
+            tamanio: 6.8,
         }, {
-            modelo: iPhone 11
-            marca: Apple
-            anio: 2019
-            color: rojo
-            memoria: 128 GB
-            tamanio: 6.1”
+            modelo: 'iPhone 11',
+            marca: 'Apple',
+            anio: 2019,
+            color: 'rojo',
+            memoria: 128,
+            tamanio: 6.1,
         }, {
-            modelo: Moto G60s
-            marca: motorola
-            anio: 2021
-            color: azul
-            memoria: 128 GB
-            tamanio: 6.78”
+            modelo: 'Moto G60s',
+            marca: 'motorola',
+            anio: 2021,
+            color: 'azul',
+            memoria: 128,
+            tamanio: 6.78,
         }, {
-            modelo: Galaxy Note 8
-            marca: Samsung
-            anio: 2017
-            color: negro
-            memoria: 256 GB
-            tamanio: 6.3
+            modelo: 'Galaxy Note 8',
+            marca: 'Samsung',
+            anio: 2017,
+            color: 'negro',
+            memoria: 256,
+            tamanio: 6.3,
         }, {
-            modelo: Galaxy Note 3
-            marca: Samsung
-            anio: 2013
-            color: blanco
-            memoria: 64 GB
-            tamanio: 5,
-            7”
+            modelo: 'Galaxy Note 3',
+            marca: 'Samsung',
+            anio: 2013,
+            color: 'blanco',
+            memoria: 64,
+            tamanio: 5.7,
         }, {
-            modelo: Galaxy S21 +
-                marca: Samsung
-            anio: 2021
-            color: negro
-            memoria: 128 GB
-            tamanio: 6.7”
+            modelo: 'Galaxy S21 +',
+            marca: 'Samsung',
+            anio: 2021,
+            color: 'negro',
+            memoria: 128,
+            tamanio: 6.7,
         }, {
-            modelo: iPhone SE
-            marca: Apple
-            anio: 2016
-            color: rose gold
-            memoria: 16 GB
-            tamanio: 4”
+            modelo: 'iPhone SE',
+            marca: 'Apple',
+            anio: 2016,
+            color: 'rose gold',
+            memoria: 16,
+            tamanio: 4,
         }, {
-            modelo: LG V30
-            marca: LG
-            anio: 2017
-            color: violeta
-            memoria: 64 GB
-            tamanio: 6”
+            modelo: 'LG V30',
+            marca: 'LG',
+            anio: 2017,
+            color: 'violeta',
+            memoria: 64,
+            tamanio: 6,
         }
     ],
     comentarios: 3
