c468ac078af2e4da33f7346ad8f7012a1f80d1ed
martinquijano5 <tutequijano@gmail.com>
2022-04-10 17:57:13 -0300

    ahora el index muestra las novedades y los mas comentados en orden aleatorizado

--- Prog2-G3/controllers/indexController.js
+++ Prog2-G3/controllers/indexController.js
@@ -1,10 +1,23 @@
 //requires necesarios
 let data = require('../db/index');
 //funciones
+function  fillArray (largo) {
+    var arr = [];
+    while(arr.length < largo){
+        var r = Math.floor(Math.random() * 10);
+        if(arr.indexOf(r) === -1) arr.push(r);
+    }
+    return arr
+}
+
+
+arrayNovedades = fillArray(4);
+arrayMasComentados = fillArray(4);
+
 
 const indexController = {
     home: function (req,res){
-        return res.render('index', {info: data});
+        return res.render('index', {info: data, arrayNovedades: arrayNovedades, arrayMasComentados: arrayMasComentados});
     }
 }
 
