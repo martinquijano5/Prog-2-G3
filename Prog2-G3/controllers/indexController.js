9ef7457639f3c870dfeb39c66851a3ac3f5d1a86
Delfina Boglione <dboglione@udesa.edu.ar>
2022-05-22 21:18:20 -0300

    index metodo conectado a la db

--- Prog2-G3/controllers/indexController.js
+++ Prog2-G3/controllers/indexController.js
@@ -12,11 +12,11 @@ let arrayMasComentados = funcionFillArray(4);
 
 const indexController = {
     home: function (req,res){
-        //phones.findAll () //corresponde a la variable qyue tiene adentro el modelo que obtuvimos de la ocnstante db, si tnego adentro el modelo podemos usar el metodo de sequelize que son promesas
-           // .then(function(calulares){
-            // res.send (celulares)
-           // }) 
-        return res.render('index', {info: data, arrayNovedades: arrayNovedades, arrayMasComentados: arrayMasComentados});
+        phones.findAll () //corresponde a la variable qyue tiene adentro el modelo que obtuvimos de la ocnstante db, si tnego adentro el modelo podemos usar el metodo de sequelize que son promesas
+              .then(function(celulares){
+                //return res.send (celulares)
+                 return res.render('index', {info: data, arrayNovedades: arrayNovedades, arrayMasComentados: arrayMasComentados});
+            }) 
     },
     search: function (req,res){
         return res.render('search-results', {info: data});
--- Prog2-G3/database/config/config.js
+++ Prog2-G3/database/config/config.js
@@ -2,22 +2,10 @@ module.exports ={
   "development": {
     "username": "root",
     "password": "root",
-    "database": "database_development",
+    "database": "phone_shopper",
     "host": "127.0.0.1",
-    "dialect": "mysql"
-  },
-  "test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
-  },
-  "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+    "dialect": "mysql",
+    "port":8889
   }
+  
 }
