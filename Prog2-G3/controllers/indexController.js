233eb0c71e655be4205d31778cbf876f3470b1d8
martinquijano5 <tutequijano@gmail.com>
2022-05-26 01:24:47 -0300

    encontre problemas en el search result cuando uso mas de un producto, primer problema el where con el parametro del query string no anda si uso el op.like y no la igualacion directa, segundo problema hacer que al res.render le llegue el array de productos y el array de usuarios

--- Prog2-G3/controllers/indexController.js
+++ Prog2-G3/controllers/indexController.js
@@ -1,5 +1,6 @@
 //requires necesarios
-const db = require('../database/models') //trae los modelos
+const db = require('../database/models'); //trae los modelos
+const { usuario } = require('../db/index');
 const phones = db.Phone //de todos los modelos pide Phone(el alias)
 const users = db.User // de todos los modelos pide User(el alias)
 const op = db.Sequelize.Op;//contiene los operadores para usar en metodos de sequelize
@@ -24,26 +25,34 @@ const indexController = {
     },
     search: function (req,res){
         phones.findAll ({
-            where: [{model: {[op.like]: '%iPhone%'}}]//solucionar hardcodeo 
+            //where: [{model: {[op.like]: req.query.search}}] //  asi deberia funcionar 
+            where: [{model: {[op.like]: "%iphone%"}}] //solucionar hardcodeo
+            //where: [{model: req.query.search}]
         })
             .then(function (celulares){
-                //return res.send (celulares) //esto lo use para ver si llegaba\
-                //agarrar perfiles de los que postearon los celulares
                 let usuarios = [];
+                let fks = [];
+                return res.send (celulares) //esto lo use para ver si llegaba -> hasta aca anda
+                //agarrar perfiles de los que postearon los celulares
                 for(let i = 0; i < celulares.length; i++){
+                    fks.push(celulares[i].FkUserId);
+                }
+                //return res.send(fks) // hasta aca anda
+                for(let i = 0; i < fks.length; i++){
                     users.findOne({
-                        where: [{id: celulares[i].FkUserId}]
+                        where: [{id: fks[i]}]
                     })
                     .then(function(usuarioPosteo){
-                        usuarios.push(usuarioPosteo);
-                        return res.render('search-results', {info: celulares, usuarios: usuarios, query: req.query.search});
-
+                        usuarios.push({id: usuarioPosteo.id, username: usuarioPosteo.username, image: usuarioPosteo.image});
+                        //return res.send(usuarios); // muestra solo el primer usuario
+                        console.log('EL USUARIO ES: ' + usuarioPosteo.username + i); // si vemos la consola vemos que el array lo recorre
                     })
                 }
-            })
-
+                //return res.send(usuarios); // aca no llegan los usuarios -> scope del then??
+                //return res.render('search-results', {info: celulares, usuarios: usuarios, query: req.query.search});
+            })        
             .catch(error => console.log('EL ERROR ES: ' + error))
-    }   
+        }
 }
 
 
